a15abd345433f91f9fdfe49da55f6af9a7402278
Pádua Neto <paduacn@gmail.com>
2024-12-11 22:23:42 -0300

    feat: add operacao financeira

--- financeiro/admin.py
+++ financeiro/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import OperacaoFinanceira, CentroDeCusto, NaturezaFinanceira
 
+@admin.register(NaturezaFinanceira)
+class NaturezaFinanceiraAdmin(admin.ModelAdmin):
+    list_display = ('codigo', "nome", "ativo")
+    search_fields = ('nome',)
+
+@admin.register(CentroDeCusto)
+class CentroDeCustoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', "nome", "ativo")
+    search_fields = ('nome',)
 # Register your models here.
+
+@admin.register(OperacaoFinanceira)
+class OperacaoFinanceiraAdmin(admin.ModelAdmin):
+    list_display=('pessoa', "data_vencimento", "valor", "pago")
+    search_fields = ("pessoa", )
+    list_filter = ("pago", "conta")
